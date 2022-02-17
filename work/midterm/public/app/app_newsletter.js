cf9714b505a0cde5f9daeee7a2a0662c8999b2d9
tangyisheng2 <tangyisheng2@sina.com>
2022-02-16 21:02:01 -0800

    Add semi-column for js

---
+++ work/midterm/public/app/app_newsletter.js
@@ -0,0 +1,35 @@
+const submitButton = document.querySelector('.newsletter__submit');
+const email = document.querySelector('#email');
+const name_ = document.querySelector('#name');
+const tier = document.querySelector('#tier');
+const form = document.querySelector('.newsletter__form');
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    let valid = true;
+    if (!email.validity.valid) {
+        email.style.border = "2px solid red";
+        valid = false;
+    }
+    if (!name_.validity.valid) {
+        name_.style.border = "2px solid red";
+        valid = false;
+    }
+    if (!tier.validity.valid) {
+        tier.style.border = "2px solid red";
+        valid = false;
+    }
+
+    if (valid) {
+        for (let elem of [email, name_, tier]) {
+            console.log(elem);
+            elem.style.border = "";
+        }
+    }
+    if (!("success" in submitButton.attributes)) {
+        if (valid) {
+            submitButton.toggleAttribute('success');
+            submitButton.innerText = "Success";
+        }
+    }
+})
