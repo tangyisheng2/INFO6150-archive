266cfdda3bee35d6a9cdfb031831d2db93dde097
tangyisheng2 <tangyisheng2@sina.com>
2022-04-13 01:16:25 -0700

    Tweak header and style

--- work/final/final/src/componeents/Header.jsx
+++ work/final/final/src/componeents/Header.jsx
@@ -1,15 +1,22 @@
-import '../css/header.css'
-function Header(){
+import '../css/header.css';
+function Header({ setShowingPage, pageData }) {
     return (
-        <header className="header">
+        <header
+            onClick={() => {
+                setShowingPage(pageData.Home);
+            }}
+            className="header"
+        >
             <mask className="icon-mask">
                 <span>📷</span>
             </mask>
             <heading className="title">
-                <h1>Gallery of California</h1>
-                <h2>Always Exploring :D</h2>
+                <a href={`#/home`}>
+                    <h1>Gallery of California</h1>
+                    <h2>Always Exploring :D</h2>
+                </a>
             </heading>
         </header>
-    )
+    );
 }
 export default Header;
