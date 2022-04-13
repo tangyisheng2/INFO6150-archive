0cb16fbdd00009777e0b3a4b4b0c3a0673f4b10d
tangyisheng2 <tangyisheng2@sina.com>
2022-04-13 01:17:02 -0700

    Add licence page

--- work/final/final/src/App.jsx
+++ work/final/final/src/App.jsx
@@ -10,21 +10,44 @@ import Monterey from './componeents/locations/Monterey';
 import SantaCruz from './componeents/locations/SantaCruz';
 import NavBar from './componeents/NavBar';
 import NotFound from './componeents/NotFound';
+import Privacy from './componeents/Privacy';
+import Licence from './componeents/License';
 
 function App() {
     const [showingPage, setShowingPage] = useState('home');
+    console.log(showingPage)
+
+    const navPageData = {
+      Home: 'home',
+      Newsletter: 'form',
+      Locations: {
+        Monterey: 'monterey',
+        "Half Moon Bay": 'halfmoonbay',
+        "Santa Cruz": 'santacruz'
+      },
+      'Comment Board': 'comments'
+    }
+
+    const miscPageData = {
+      "Privacy Policy": 'privacy',
+      "Licence": 'licence'
+    }
 
     return (
         <div className="app">
-            <Header className="header" />
+            <Header setShowingPage={setShowingPage} pageData={navPageData} className="header" />
             <NavBar className="nav" />
             {showingPage === 'home' && <Home />}
-            {showingPage === 'form' && <Form />}
+            {showingPage === 'form' && <Form pageData={navPageData}/>}
             {showingPage === 'NotFound' && <NotFound />}
             {showingPage === 'monterey' && <Monterey />}
             {showingPage === 'halfmoonbay' && <HalfMoonBay />}
             {showingPage === 'santacruz' && <SantaCruz />}
-            <Footer className="footer" />
+            {showingPage === 'privacy' && <Privacy />}
+            {showingPage === 'licence' && <Licence />}
+            {/* {!(showingPage in [...Object.keys(navPageData), ...Object.keys(miscPageData)] || showingPage === undefined) && <NotFound setShowingPage={setShowingPage} pageData={navPageData} className='404'/>} */}
+            <Footer setShowingPage={setShowingPage} pageData={miscPageData} className="footer" />
+            {/* <Footer className="footer" /> */}
         </div>
     );
 }
