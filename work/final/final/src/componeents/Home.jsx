e8f50634d7562f63a15bdeaffc4321fdac6765be
tangyisheng2 <tangyisheng2@sina.com>
2022-04-12 23:54:38 -0700

    add styles for home

--- work/final/final/src/componeents/Home.jsx
+++ work/final/final/src/componeents/Home.jsx
@@ -1,18 +1,16 @@
+import '../css/home.css'
 function Home() {
     return (
         <main class="main">
-            <div class="welcome__message">
-                <p>👋Hi, welcome to my gallery.</p>
-                <p>👨‍🎓I am Eason from Northeastern Univerisity</p>
-                <p>⌨️I am mostly a programmer</p>
-                <p>
-                    📷But this site is mostly used to share some of my
-                    photographies
-                </p>
-                <p>😄Feel free to look around</p>
-                <p>📺And don't forget to subscribe my socal media!</p>
-                <p>📰and also the Newsletter</p>
-            </div>
+            <p>👋Hi, welcome to my gallery.</p>
+            <p>👨‍🎓I am Eason from Northeastern Univerisity</p>
+            <p>⌨️I am mostly a programmer</p>
+            <p>
+                📷But this site is mostly used to share some of my photographies
+            </p>
+            <p>😄Feel free to look around</p>
+            <p>📺And don't forget to subscribe my socal media!</p>
+            <p>📰and also the Newsletter</p>
         </main>
     );
 }
