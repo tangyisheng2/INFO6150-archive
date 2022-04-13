f849b4cf960eb44f2cea9143332fc24e3d009762
tangyisheng2 <tangyisheng2@sina.com>
2022-04-12 23:37:47 -0700

    Add home content

--- work/final/final/src/componeents/Home.jsx
+++ work/final/final/src/componeents/Home.jsx
@@ -1,6 +1,19 @@
-function Home () {
+function Home() {
     return (
-        <div className="home">I am home</div>
-    )
+        <main class="main">
+            <div class="welcome__message">
+                <p>👋Hi, welcome to my gallery.</p>
+                <p>👨‍🎓I am Eason from Northeastern Univerisity</p>
+                <p>⌨️I am mostly a programmer</p>
+                <p>
+                    📷But this site is mostly used to share some of my
+                    photographies
+                </p>
+                <p>😄Feel free to look around</p>
+                <p>📺And don't forget to subscribe my socal media!</p>
+                <p>📰and also the Newsletter</p>
+            </div>
+        </main>
+    );
 }
 export default Home;
