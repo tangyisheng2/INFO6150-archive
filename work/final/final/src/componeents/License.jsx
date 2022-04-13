aa524ce107102a2aa347a1d69eccdb46285ad0f7
tangyisheng2 <tangyisheng2@sina.com>
2022-04-13 01:16:46 -0700

    Add licence page

---
+++ work/final/final/src/componeents/License.jsx
@@ -0,0 +1,16 @@
+import '../css/home.css';
+function Licence() {
+    return (
+        <main className="main">
+            <div className="licence__image">
+                <p>Image are licensed under the CC BY-NC-SA 4.0 License.</p>
+                Copyright 1999-2022 Yisheng Tang.<p></p>{' '}
+            </div>
+            <div className="css.gg__image">
+                <p>Spinner are licensed under The MIT License (MIT).</p>{' '}
+                <p>Copyright © 2019-2020 css.gg</p>
+            </div>
+        </main>
+    );
+}
+export default Licence;
