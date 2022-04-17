0b436b6ccf37217738d9deee064d2c62839a2e67
tangyisheng2 <tangyisheng2@sina.com>
2022-04-16 22:57:23 -0700

    style: <NavBar> changed to double quote

--- work/final/final/src/componeents/NavBar.jsx
+++ work/final/final/src/componeents/NavBar.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import ShowObject from './utils/ShowObject';
-import '../css/navbar.css'
+import '../css/navbar.css';
 
 function NavBar({ data, setShowingPage }) {
     const [expandedMenu, setExpandedMenu] = useState({});
 
     return (
-        <nav className='navbar'>
+        <nav className="navbar">
             <ShowObject
                 entry={data}
                 setShowingPage={setShowingPage}
