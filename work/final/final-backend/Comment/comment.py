81ced9878a4b0336e544a68fe3217aecf7f6a713
tangyisheng2 <tangyisheng2@sina.com>
2022-04-20 00:11:32 -0700

    chore: <comment> refine code

--- work/final/final-backend/Comment/comment.py
+++ work/final/final-backend/Comment/comment.py
@@ -11,24 +11,6 @@ class Comment(Resource):
     comment = []
 
     def __init__(self, **kwargs):
-        # if not self.comment:
-        #     self.comment = [
-        #         {
-        #             "id": 1,
-        #             "name": "admin",
-        #             "body": "Hello World!"
-        #         },
-        #         {
-        #             "id": 2,
-        #             "name": "Zeoob",
-        #             "body": "Omg, is it real? Start building your own."
-        #         },
-        #         {
-        #             "id": 3,
-        #             "name": "Mike",
-        #             "body": "This is by far one of the coolest things I’ve ever seen."
-        #         }
-        #     ]
         self.comment = kwargs["comment"]
         self.index = len(self.comment) + 1 if self.comment else 1
 
