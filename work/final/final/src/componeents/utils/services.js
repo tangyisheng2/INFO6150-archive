b39f87aec42cba3ae565ea83610fcc6c926d9064
tangyisheng2 <tangyisheng2@sina.com>
2022-04-19 18:56:03 -0700

    feat: <services> Add post comment service

--- work/final/final/src/componeents/utils/services.js
+++ work/final/final/src/componeents/utils/services.js
@@ -3,7 +3,8 @@ export function fetchComments() {
     const endPointUrl = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
     return new Promise((resolve) => {
         setTimeout(resolve, 2000);
-    }).catch(() => Promise.reject('networkError'))
+    })
+        .catch(() => Promise.reject('networkError'))
         .then(() =>
             fetch(endPointUrl, {
                 method: 'GET',
@@ -12,11 +13,27 @@ export function fetchComments() {
         .catch(() => Promise.reject('networkError'))
         .then((response) => {
             if (response.ok) {
-                // const result = response.json();
-                // console.log(response.json());
+                const result = response.json();
+                console.log(result);
+                return result;
                 // return response.json();
-                // console.log(result)
-                return response.json()
+            }
+            return Promise.reject('serviceError');
+        });
+}
+
+export function postComments(commentObj) {
+    const endPointUrl = `https://httpbin.org/post`;
+    const postBody = JSON.stringify(commentObj);
+    return fetch(endPointUrl, {
+        method: 'POST',
+        body: postBody,
+    })
+        .catch(() => Promise.reject('networkError'))
+        .then((response) => {
+            if (response.ok) {
+                const result = response.json();
+                return result;
             }
             return Promise.reject('serviceError');
         });
